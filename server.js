f95554e9ecff9d08e57cbabece77b34b5024e76f
jingram3 <jstalvern@gmail.com>
2019-03-23 15:06:51 -0400

    preparing for deployment

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 let players = {};
 
